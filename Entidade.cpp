648e902a11c0d4b80b4f7c75c89b4ce9c234a3ab
micheleotta <131482012+micheleotta@users.noreply.github.com>
2023-10-21 18:02:46 -0300

    Update Entidade.cpp

--- Entidade.cpp
+++ Entidade.cpp
@@ -1,15 +1,13 @@
 #include "Entidade.h"
+using namespace Entidades; 
 
-using namespace Entidades {
+Entidade::Entidade(): Ente(), x(0), y(0){
+}
 
-	Entidade::Entidade(): Ente(), x(0), y(0){
-	}
+int Entidade::getX() {
+	return x;
+}
 
-	int Entidade::getX() {
-		return x;
-	}
-
-	int Entidade::getY() {
-		return y;
-	}
+int Entidade::getY() {
+	return y;
 }

1edc556bc4ca03f7c0d07763898f0e12f08f1fa9
micheleotta <131482012+micheleotta@users.noreply.github.com>
2023-10-21 18:05:23 -0300

    Update Projetil.cpp

--- Projetil.cpp
+++ Projetil.cpp
@@ -1,19 +1,17 @@
 #include "Projetil.h"
+using namespace Entidades;
 
-using namespace Entidades {
-	Projetil::Projetil(): Entidade(), velocidade(0) {
+Projetil::Projetil(): Entidade(), velocidade(0) {
+}
 
-	}
+Projetil::~Projetil() {
 
-	Projetil::~Projetil() {
+}
 
-	}
+void Projetil::executar() {
 
-	void Projetil::executar() {
+}
 
-	}
+void Projetil::salvar() {
 
-	void Projetil::salvar() {
-
-	}
 }

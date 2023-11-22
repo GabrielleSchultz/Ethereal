38d9ee16bf3c1672fd39dc720cba05bd495def68
micheleotta <micheleotta@gmail.com>
2023-11-22 14:34:04 -0300

    static no Ente

--- Ente.cpp
+++ Ente.cpp
@@ -1,10 +1,10 @@
 #include "Ente.h"
 
 int Ente::cont_id(0);
+Gerenciadores::Grafico* Ente::pGerenciadorGrafico(Gerenciadores::Grafico::getGerenciador_Grafico());
 
-
-Ente::Ente() : _id(cont_id),
-pGerenciadorGrafico(Gerenciadores::Grafico::getGerenciador_Grafico())
+Ente::Ente() : 
+	_id(cont_id)
 {
 	cont_id++;
 }
